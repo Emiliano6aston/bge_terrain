a4054496ff35c1f32db5d783e60e1ffd422d269a
Porteries Tristan <republicthunderbolt9@gmail.com>
2015-01-28 16:02:22 +0100

    implementation de lod

--- source/gameengine/Ketsji/KX_Chunk.cpp
+++ source/gameengine/Ketsji/KX_Chunk.cpp
@@ -2,17 +2,18 @@
 #include "KX_Terrain.h"
 #include "KX_Camera.h"
 
+#include "KX_PythonInit.h"
+
 #include "RAS_IRasterizer.h"
 
 #define DEBUG(msg) std::cout << msg << std::endl;
 
-KX_Chunk::KX_Chunk(vector2DInt pos, KX_Terrain *terrain, RAS_MaterialBucket* bucket, int id)
-	:m_id(id),
-	m_position(pos),
+KX_Chunk::KX_Chunk(vector2DInt pos, KX_Terrain *terrain)
+	:m_position(pos),
 	m_terrain(terrain),
 	m_meshSlot(NULL),
-	m_material(bucket),
-	m_lastSubDivision(0)
+	m_lastSubDivision(0),
+	m_culled(false)
 {
 // 	DEBUG("Create chunk, pos : " << pos.m_x << ", " << pos.m_y << ", id : " << id);
 	//On crée la bwate
@@ -25,14 +26,14 @@ KX_Chunk::KX_Chunk(vector2DInt pos, KX_Terrain *terrain, RAS_MaterialBucket* buc
 	const float size=m_terrain->GetChunkSize();
 	const float height=m_terrain->GetMaxHeight();
 	m_box[0]=MT_Point3(pos.m_x * size, pos.m_y * size, 0);
-	m_box[0]=MT_Point3(pos.m_x * size + size, pos.m_y * size, 0);
-	m_box[0]=MT_Point3(pos.m_x * size + size, pos.m_y * size + size, 0);
-	m_box[0]=MT_Point3(pos.m_x * size, pos.m_y * size + size, 0);
-	m_box[0]=MT_Point3(pos.m_x * size, pos.m_y * size, height);
-	m_box[0]=MT_Point3(pos.m_x * size + size, pos.m_y * size, height);
-	m_box[0]=MT_Point3(pos.m_x * size + size, pos.m_y * size + size, height);
-	m_box[0]=MT_Point3(pos.m_x * size, pos.m_y * size + size, height);
-	m_subDivisions = (rand() % 2) ? 8 : 16;
+	m_box[1]=MT_Point3(pos.m_x * size + size, pos.m_y * size, 0);
+	m_box[2]=MT_Point3(pos.m_x * size + size, pos.m_y * size + size, 0);
+	m_box[3]=MT_Point3(pos.m_x * size, pos.m_y * size + size, 0);
+	m_box[4]=MT_Point3(pos.m_x * size, pos.m_y * size, height);
+	m_box[5]=MT_Point3(pos.m_x * size + size, pos.m_y * size, height);
+	m_box[6]=MT_Point3(pos.m_x * size + size, pos.m_y * size + size, height);
+	m_box[7]=MT_Point3(pos.m_x * size, pos.m_y * size + size, height);
+	m_subDivisions = (rand() % 2) ? 1 : 2;
 }
 
 KX_Chunk::~KX_Chunk()
@@ -51,10 +52,10 @@ void KX_Chunk::Update(KX_Camera* cam)
 	m_culled = IsCulled(cam);
 	const float size = m_terrain->GetChunkSize();
 	const float distance = MT_Point3(m_position.m_x * size, m_position.m_y * size, 0.).distance2(cam->NodeGetWorldPosition());
-// 	m_subDivisions = m_terrain->GetSubdivision(0.);
+// 	m_subDivisions = m_terrain->GetSubdivision(distance);
 }
 
-void KX_Chunk::UpdateDisplayArrayDraw(const MT_Transform& cameratrans, RAS_IRasterizer* rasty)
+void KX_Chunk::UpdateDisplayArrayDraw(RAS_IRasterizer* rasty)
 {
 	if (m_culled)
 		return;
@@ -66,7 +67,7 @@ void KX_Chunk::UpdateDisplayArrayDraw(const MT_Transform& cameratrans, RAS_IRast
 			delete m_meshSlot;
 
 		m_meshSlot = new RAS_MeshSlot();
-		m_meshSlot->init(m_material, 4);//temporairement on utilise des carrés
+		m_meshSlot->init(NULL, 4);//temporairement on utilise des carrés
 
 		// tous les chunk adjacent
 		KX_Chunk* chunkLeft = m_terrain->GetChunk(m_position.m_x - 1, m_position.m_y);
@@ -79,6 +80,14 @@ void KX_Chunk::UpdateDisplayArrayDraw(const MT_Transform& cameratrans, RAS_IRast
 		const bool jointRight = chunkRight ? chunkRight->GetSubDivision() < m_subDivisions : false;
 		const bool jointFront = chunkFront ? chunkFront->GetSubDivision() < m_subDivisions : false;
 		const bool jointBack = chunkBack ? chunkBack->GetSubDivision() < m_subDivisions : false;
+		if (jointLeft && (chunkLeft->GetSubDivision() - m_subDivisions) > 1)
+			DEBUG("joint left greater than one");
+		if (jointRight && (chunkRight->GetSubDivision() - m_subDivisions) > 1)
+			DEBUG("joint right greater than one");
+		if (jointFront && (chunkFront->GetSubDivision() - m_subDivisions) > 1)
+			DEBUG("joint front greater than one");
+		if (jointBack && (chunkBack->GetSubDivision() - m_subDivisions) > 1)
+			DEBUG("joint back greater than one");
 
 		//La taille "réelle" du chunk
 		const float size = m_terrain->GetChunkSize();
@@ -90,7 +99,7 @@ void KX_Chunk::UpdateDisplayArrayDraw(const MT_Transform& cameratrans, RAS_IRast
 		const MT_Point2 realPos(m_position.m_x * size, m_position.m_y * size);
 
 		// la liste contenant toutes les colonnes qui seront relus plus tard pour créer des faces
-		unsigned short columnList[m_subDivisions+1][m_subDivisions+1];
+		short columnList[m_subDivisions+1][m_subDivisions+1];
 		unsigned int columnIndex = 0;
 
 		// on construit tous les vertices et on stocke leur numero dans des colonnes
@@ -99,43 +108,56 @@ void KX_Chunk::UpdateDisplayArrayDraw(const MT_Transform& cameratrans, RAS_IRast
 			unsigned int vertexIndex = 0;
 			for(float y = realPos.y(); y <= realPos.y() + size; y += interval)
 			{
-				float height = 0.;
-				if (((jointFront && vertexIndex == m_subDivisions) || (jointBack && vertexIndex == 0)) && (columnIndex % 2))
-					height = (BLI_hnoise(10., x - interval, y, 0.) + BLI_hnoise(10., x + interval, y, 0.)) / 2.;
-				else if (((jointRight && columnIndex == m_subDivisions) || (jointLeft && columnIndex == 0)) && (vertexIndex % 2))
-					height = (BLI_hnoise(10., x, y - interval, 0.) + BLI_hnoise(10., x, y + interval, 0.)) / 2.;
+				if ((((jointFront && vertexIndex == m_subDivisions) || 
+					(jointBack && vertexIndex == 0)) 
+					&& (columnIndex % 2)) ||
+					(((jointRight && columnIndex == m_subDivisions) || 
+					(jointLeft && columnIndex == 0)) 
+					&& (vertexIndex % 2)))
+				{
+					// on indique que ce vertice est inexistant car il y a une jointure
+					columnList[columnIndex][vertexIndex++] = -1; 
+				}
 				else
-					height = BLI_hnoise(10., x, y, 0.);
-
-				//Remplissage de l'uv lambda:
-				MT_Point2 uvs_buffer[8] = {MT_Point2(x, y)};
-				//bourrage de vertices
-				columnList[columnIndex][vertexIndex++] = m_meshSlot->AddVertex(
-														RAS_TexVert(MT_Point3(x, y, -height * maxheight),
+				{
+					//Remplissage de l'uv lambda:
+					MT_Point2 uvs_buffer[8] = {MT_Point2(x, y)};
+					//bourrage de vertices
+					columnList[columnIndex][vertexIndex++] = m_meshSlot->AddVertex(
+														RAS_TexVert(MT_Point3(x, y, BLI_hnoise(10., x, y, 0.) * maxheight),
 														uvs_buffer,
 														MT_Vector4(1., 1., 1., 1.),
 														0, // Couleur + transparence
 														MT_Vector3(0, 0, 1.),
 														true, 0));
+				}
 			}
 			++columnIndex;
 		}
 
 		// on lit toutes les colonnes pour créer des faces
-		for (unsigned int i = 0; i < m_subDivisions; ++i)
+		for (unsigned int columnIndex = 0; columnIndex < m_subDivisions; ++columnIndex)
 		{
-			for (unsigned int j = 0; j < m_subDivisions; ++j)
+			for (unsigned int vertexIndex = 0; vertexIndex < m_subDivisions; ++vertexIndex)
 			{
-				m_meshSlot->AddPolygon(4);
-				m_meshSlot->AddPolygonVertex(columnList[i][j]);
-				m_meshSlot->AddPolygonVertex(columnList[i][j+1]);
-				m_meshSlot->AddPolygonVertex(columnList[i+1][j+1]);
-				m_meshSlot->AddPolygonVertex(columnList[i+1][j]);
+				m_meshSlot->AddPolygon(3); // TODO
+				m_meshSlot->AddPolygonVertex(columnList[columnIndex][vertexIndex]);
+				m_meshSlot->AddPolygonVertex(columnList[columnIndex][vertexIndex+1]);
+				m_meshSlot->AddPolygonVertex(columnList[columnIndex+1][vertexIndex+1]);
+				m_meshSlot->AddPolygonVertex(columnList[columnIndex+1][vertexIndex]);
 			}
 		}
 	}
-	while (m_material->ActivateMaterial(cameratrans, rasty))
+	if (m_meshSlot)
 		rasty->IndexPrimitives(*m_meshSlot);
+
+	/*for (int i = 0; i < 4; i++)
+	{
+		static MT_Vector3 color(1., 1., 1.);
+		KX_RasterizerDrawDebugLine(m_box[i], m_box[i + 4], color);
+		KX_RasterizerDrawDebugLine(m_box[i], m_box[(i < 3) ? i + 1 : 0], color);
+		KX_RasterizerDrawDebugLine(m_box[i + 4], m_box[(i < 3) ? i + 5 : 4], color);
+	}*/
 }
 
 void KX_Chunk::EndUpdate()
